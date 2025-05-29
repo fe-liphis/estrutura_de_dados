04062b8e76fb8492bcdef96cca4ad3b85389760c
fe-liphis <felipeg.costaoliveira@gmail.com>
2025-05-29 19:33:19 -0300

    FEAT: implementação do conceito de Fila

---
+++ Fila.py
@@ -0,0 +1,35 @@
+from collections import deque 
+
+fila = deque()
+senha = 1
+
+print("Bem vindo ao sistema!")
+
+while True:
+    print("Escolha uma opção abaixo:")
+    print("1 - Retirar senha")
+    print("2 - Ver fila")
+    print("3 - Chamar senha")
+    print("4 - Sair")
+    
+    opcao = int(input())
+
+    if opcao == 1:
+        fila.append(f"A{senha}")
+        print(f"Senha retirada: A{senha}")
+        senha += 1
+    elif opcao == 2:
+        print(list(fila))
+    elif opcao == 3:
+        if fila:
+            senhaChamada = fila.popleft()
+            print(f"Senha chamada: {senhaChamada}")
+        else:
+            print("Fila vazia")
+    elif opcao == 4:
+        print("Obrigado por utilizar o sistema!!!")
+        break
+    else:
+        print("Opção inválida")
+
+    print("\n")
